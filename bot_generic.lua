8932ed52b81e59a97e75aed1a4daba2d1879bf17
Martin Evans <martindevans@gmail.com>
2016-12-13 04:28:09 +0000

    - Added item_purchase_generic.lua which prevents bots from using the default purchasing behaviour  - Dumped everything in the _G table (handy reference)
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- bot_generic.lua
+++ bot_generic.lua
@@ -1,3 +1,11 @@
-function Think()
+local state = 0;
 
+function Think()
+    if state == 0 then
+        for k, _ in pairs(_G) do
+            print(k);
+        end
+    end
+    
+    state = state + 1;
 end
---
+++ item_purchase_generic.lua
@@ -0,0 +1,4 @@
+function ItemPurchaseThink()
+    --Leave this empty.
+    --The existance of this function in this file prevents the default purchasing behaviour from happening.
+end
